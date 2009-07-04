b26e8c2be243622a83c4b272465fa773ed163965
Guru Mahendran <mr.craigy@gmail.com>
2009-07-04 15:53:38 -0400

    Cleaned up repo.

--- SkuleClubs/app/models/event.rb
+++
@@ -1,4 +0,0 @@
-class Event < ActiveRecord::Base
-  belongs_to      :club
-  belongs_to      :user
-end
--- SkuleClubs/app/models/group.rb
+++
@@ -1,5 +0,0 @@
-class Group < ActiveRecord::Base
-  belongs_to      :club
-  has_many        :users,   :through => :memberships
-  #better nested tree
-end
