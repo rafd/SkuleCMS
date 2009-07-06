af439eb2473c1b9a6e7c6630f3149981d25a2ef0
Rafal Dittwald <rafal.dittwald@gmail.com>
2009-07-05 22:37:51 -0400

    minor fix to push to heroku

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,5 +1,8 @@
-require 'compass'
-# If you have any compass plugins, require them here.
-Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.config"))
-Compass.configuration.environment = RAILS_ENV.to_sym
-Compass.configure_sass_plugin!
+if RAILS_ENV == "development"
+	require 'compass'
+	# If you have any compass plugins, require them here.
+	Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.config"))
+	Compass.configuration.environment = RAILS_ENV.to_sym
+	Sass::Plugin.options[:never_update] = true
+	Compass.configure_sass_plugin!
+end
