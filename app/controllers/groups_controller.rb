b1a4290634748e056842dc95f9e77860e44f3097
Raymond Lam <here.is.raymond.lam@gmail.com>
2009-07-14 21:10:32 -0400

    Groups now uses the betternestedset plugin for organization. Added a simple display for the nested format of groups. Added the membership model which is required for the :through relations to work. Fixed page.name ref to page.title in the club's _menu file.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,14 @@
 class GroupsController < ApplicationController
+  before_filter :load_club
+  def load_club
+    @club = Club.find(params[:club_id])
+  end
+  
   # GET /groups
   # GET /groups.xml
   def index
-    @groups = Group.all
-
+    @groups = @club.groups
+    @roots = Group.find(:all, :conditions => {:club_id => @club, :parent_id => nil})
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @groups }
@@ -25,7 +30,7 @@ class GroupsController < ApplicationController
   # GET /groups/new.xml
   def new
     @group = Group.new
-
+    @grouplist = @club.groups
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @group }
@@ -35,17 +40,21 @@ class GroupsController < ApplicationController
   # GET /groups/1/edit
   def edit
     @group = Group.find(params[:id])
+    @grouplist = @club.groups
   end
 
   # POST /groups
   # POST /groups.xml
   def create
     @group = Group.new(params[:group])
-
+    @group.club = @club;
     respond_to do |format|
       if @group.save
+        if !params[:group][:parent_id].blank?
+          @group.move_to_child_of (Group.find (params[:group][:parent_id]))
+        end
         flash[:notice] = 'Group was successfully created.'
-        format.html { redirect_to(@group) }
+        format.html { redirect_to club_group_path(@club, @group) }
         format.xml  { render :xml => @group, :status => :created, :location => @group }
       else
         format.html { render :action => "new" }
@@ -61,8 +70,11 @@ class GroupsController < ApplicationController
 
     respond_to do |format|
       if @group.update_attributes(params[:group])
+        if !params[:group][:parent_id].blank?
+          @group.move_to_child_of (Group.find (params[:group][:parent_id]))
+        end
         flash[:notice] = 'Group was successfully updated.'
-        format.html { redirect_to(@group) }
+        format.html { redirect_to club_group_path(@club, @group) }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
@@ -78,7 +90,7 @@ class GroupsController < ApplicationController
     @group.destroy
 
     respond_to do |format|
-      format.html { redirect_to(groups_url) }
+      format.html { redirect_to(club_groups_url) }
       format.xml  { head :ok }
     end
   end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,12 @@
 class Group < ActiveRecord::Base
   belongs_to      :club
+  has_many        :memberships
   has_many        :users,   :through => :memberships
   #better nested tree
+
+  
+  acts_as_nested_set  :parent_column => "parent_id",
+                      :left_column => "lft",
+                      :right_column => "rgt",
+                      :text_coloumn => "name"
 end
