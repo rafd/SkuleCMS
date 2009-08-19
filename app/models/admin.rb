43b0a3622946dc4aac9b8eb6b89b928b74b10a32
Raymond Lam <here.is.raymond.lam@gmail.com>
2009-08-19 19:47:55 -0400

    Added attr_accessible to models. Only two levels of pages are shown on left side.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,5 +4,7 @@ class Admin < ActiveRecord::Base
     c.validates_format_of_login_field_options = {:with => /^[A-Za-z\d_]+$/, :message => "name is invalid. Only letters, numbers, and underscores allowed."}
     c.validates_length_of_password_field_options = {:minimum => 6, :if => :require_password?}
   end
-
+  
+  attr_accessible :login, :password, :password_confirmation
+  
 end
--- app/models/club.rb
+++ app/models/club.rb
@@ -10,8 +10,9 @@ class Club < ActiveRecord::Base
   has_many      :small_posts, :dependent => :destroy
   has_many      :large_posts, :dependent => :destroy
 
-  validates_presence_of     :name, :description, :official_name, :tagline, :web_name
-  validates_uniqueness_of   :name, :official_name, :web_name
+  validates_uniqueness_of   :name, :case_sensitive => false
+  validates_uniqueness_of   :official_name, :case_sensitive => false
+  validates_uniqueness_of   :web_name, :case_sensitive => false
   validates_length_of       :name, :maximum => 20
   validates_length_of       :official_name, :maximum => 100
   validates_length_of       :contact, :maximum => 50
@@ -20,7 +21,9 @@ class Club < ActiveRecord::Base
   validates_length_of       :tagline, :in => 5..100
   validates_length_of       :description, :in => 5..400
   validates_format_of       :web_name, :with => /^[A-Za-z\d_]+$/, :message => "name is invalid. Only letters, numbers, and underscores allowed."
-  validates_format_of       :contact, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => 'email is invalid.'
+  validates_format_of       :contact, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :allow_blank => true, :message => 'email is invalid.'
+  
+  attr_accessible :name, :description, :official_name, :tagline, :web_name, :contact, :address, :live
   
   attr_accessor :logo, :banner
   
@@ -90,7 +93,7 @@ class Club < ActiveRecord::Base
     @page = self.pages.new
     @page.title = "About Us"
     @page.content = self.description
-    @page.order = "3"
+    @page.order = "1"
     @page.parent_id = @root_page.id
     @page.save
     
@@ -104,7 +107,7 @@ class Club < ActiveRecord::Base
     @page = self.pages.new
     @page.title = "Constitution"
     @page.content = ""
-    @page.order = "1"
+    @page.order = "3"
     @page.parent_id = @root_page.id
     @page.save
     
@@ -121,11 +124,24 @@ class Club < ActiveRecord::Base
   end
   
   def member_list
-    return self.groups.find(:first, :conditions => {:parent_id => nil})
+    @group = self.groups.find(:first, :conditions => {:parent_id => nil})
+    if @group.blank?
+      @group = Group.new
+      @group.club_id = self.id
+      @group.name = "Member List"
+      @group.misc = "Full member list of the club"
+      @group.save
+    end
+    return @group
   end
   
   def root_page
-    return self.pages.find(:first, :conditions => {:parent_id => nil})
+    @root_page = self.pages.find(:first, :conditions => {:parent_id => nil})
+    if @root_page.blank?
+      @root_page = self.pages.new
+      @root_page.save_with_validation(false)
+    end
+    return @root_page
   end
   
   def members
