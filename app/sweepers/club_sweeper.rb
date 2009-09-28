2cefd20f52874df963dbf3bf934cd82c168ca9ed
Raymond Lam <here.is.raymond.lam@gmail.com>
2009-09-28 00:26:30 -0400

    Added calendar cache expiration

--- app/sweepers/club_sweeper.rb
+++ app/sweepers/club_sweeper.rb
@@ -1,16 +1,25 @@
 class ClubSweeper < ActionController::Caching::Sweeper
   observe Club
   
+  def after_save(club)
+    expire_calendar(club)
+  end
+  
   def after_update(club)
     expire_cache(club)
   end
   
   def after_destroy(club)
     expire_cache(club)
+    expire_calendar(club)
   end
   
   def expire_cache(club)
     expire_page(:controller => 'clubs', :action => 'index')
     expire_page(:controller => 'hub_pages', :action => 'services')
+  end
+  
+  def expire_calendar(club)
+    expire_page(:controller => 'hub_pages', :action => 'calendar')
   end
 end
